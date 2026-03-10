277502e717e5d8e0928294d021d9d0510114dec1
Aidar <ajobsforme@gmail.com>
2026-03-10 12:31:42 +0300

    feat: openapi

--- twitter_analog/app/main.py
+++ twitter_analog/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from posts import routes as posts_routes
 
-app = FastAPI()
+app = FastAPI(
+    title="Posts API",
+    description="API для работы приложения аналога Twitter",
+    version="0.0.1",
+    openapi_tags=[{"name": "Posts", "description": "Управление постами"}],
+)
 app.include_router(posts_routes.router)
