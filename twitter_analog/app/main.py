f422649fc87cd75663e04611090b941ee1369650
Aidar <ajobsforme@gmail.com>
2026-03-18 10:11:51 +0300

    fix: добавил настройки к приложению

--- twitter_analog/app/main.py
+++ twitter_analog/app/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from app.posts.routes import router as posts_router
+from app.core.settings import Settings
 
 
 def create_app() -> FastAPI:
+    settings = Settings()  # type: ignore[call-arg]
     new_app = FastAPI(
         title="Posts API",
         description="API для работы приложения аналога Twitter",
         version="0.0.1",
         openapi_tags=[{"name": "Posts", "description": "Управление постами"}],
     )
+    new_app.state.settings = settings
 
     new_app.include_router(posts_router)
 
